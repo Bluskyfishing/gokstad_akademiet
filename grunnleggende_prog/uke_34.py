b07b004e7cf9779496558eb375465a3c34e6f7d4
root <root@Fishas>
2023-08-24 16:09:30 +0200

    oppretter fagmapper

---
+++ grunnleggende_prog/uke_34.py
@@ -0,0 +1,64 @@
+#Kode fra tavla: 
+#print(f'{2} + {3} = {2+3}')
+#
+#enere = 3 
+#toere = 6 
+#summen = enere + toere
+#
+#print(f"Summen av enere og toere er {summen}.")
+#
+#Oppgaver:
+#1
+#navn = input(str("Hei! Hva er navnet ditt?: "))
+#
+#print(f"Hei {navn} hvordan går det?")
+#
+#2
+#svar1 = input("Skriv inn et tall: ")
+#svar2 = input("Skriv inn et tall till: ")
+#summen = int(svar1) + int(svar2) 
+#print(f"Summen er {summen}.")
+
+#3
+#tall1 = int(input("Skriv inn tall: "))
+#tall2 = int(input("Skriv inn et annent tall: "))
+#sum1 = tall1 + tall2
+#print(f"gjennomsnittet mellom tallene er: {sum1/2}")
+
+#4
+#temp = float(input("Skriv inn temperatur i celcius: "))
+#farenheit = temp * (9/5) + 32 
+#print(f"Temperaturen er °{farenheit}.")
+
+#5 Ikke løst
+#minutter = int(input("Angi antall minutter: "))
+#konverter = minutter /60 
+#print(f"{konverter} timer og {konverter} minutter. ")
+
+#6
+#teller = len(input("Skriv et ord: "))
+#print(f"Ordet har {teller} bokstaver i seg.")
+
+#7
+#bursdag= int(input("Hva er fødselsdatoen din? "))
+#alder = 2023 - bursdag 
+#print(f"Du er {alder} år gammel.")
+
+#8 Kan kutte noe noen desimaler muligens.
+#pris = int(input("Skriv inn pris på vare: "))
+#total = pris * 1.15 
+#tips = total * 0.15
+#print(f"Tips:{tips} Total: {total}")
+
+#9
+#ord = input("Skriv inn et ord: ")
+#print(ord[::-1])
+
+#10 uten if?????? 
+#ord = input("Skriv inn et ord: ")
+
+#11
+vekt = int(input("Vekt i Kg: "))
+høyde = float(input("Høyde i M: "))
+bmi = høyde*høyde 
+print(f"Bmi-en din er: {bmi}.")
