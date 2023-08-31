6847600f178ada620183b8a4419b93a21d369031
Bluskyfishing <blusky445@gmail.com>
2023-08-31 17:30:20 +0200

    Gjort mer på Diverse oppgaver.

--- grunnleggende_prog/35_uke/diverse_oppgaver.py
+++ grunnleggende_prog/35_uke/diverse_oppgaver.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot
+#from matplotlib import pyplot
 #a)
 #x =["Volvo", "BMW", "Opel", "Ford", "Tesla","Nissan","Hyundai"]
 #y = [1,3,6,7,6,9,4]
@@ -21,7 +21,6 @@ from matplotlib import pyplot
 
 
 #Oppgave 3 
-#a 
 karakter_liste = []
 teller = 0
 
@@ -30,9 +29,18 @@ karakterer = None
 while karakterer != "stop":
     karakterer = str(input(f"Skriv 'stop' når alle karaterene er lagt inn. \nSkriv inn karakter: "))
     if karakterer == "stop":
-        int_liste = list(map(int,karakter_liste)) #???? 
-        print(int_liste)
+        int_liste = list(map(int,karakter_liste)) #????
+        int_liste.sort() 
+
+        gjennomsnitt = sum(int_liste) / len(int_liste)
+
+        median = len(int_liste)/2
+        median2 = int_liste[int(median)]
+
+        print(f"\nDet ble lagt inn {teller} karakterer.")
+        print(f"Den laveste karakteren:{int_liste[0]}\nDen høyeste karakteren:{int_liste[-1]}\nGjennomsnitt karakteren:{gjennomsnitt}")
+        print(f"Median karakter:{median2}")
         break
     else:
+        teller += 1
         karakter_liste.append(karakterer)
-        
