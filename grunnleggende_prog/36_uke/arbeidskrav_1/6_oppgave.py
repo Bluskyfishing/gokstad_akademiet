994e0b4cd6aa3177f29b0f7bc2e5c8a41ed656e6
Bluskyfishing <blusky445@gmail.com>
2023-09-05 01:07:45 +0200

    while loop implementert i oppgave 6.

--- grunnleggende_prog/36_uke/arbeidskrav_1/6_oppgave.py
+++ grunnleggende_prog/36_uke/arbeidskrav_1/6_oppgave.py
@@ -2,7 +2,6 @@
 #Beskrivelse: 
 
 #Lag en Meny:
- 
 #Programmet skal starte med å vise en meny med følgende valgmuligheter: 
 #Addisjon (Pluss) 
 #Subtraksjon (Minus) 
@@ -12,35 +11,44 @@
 
 #Brukerinput for Valg:  
 #Etter at menyen er vist, skal programmet vente på at brukeren gjør et valg. 
+valg = 0
 
-valg = float(input(f"Velg et av følgene valg:\n1.Addisjon\n2.Subtraksjon\n3.Multiplikasjon\n4.Divisjon\n5.Avslutt\nSkriv her: "))
+while valg != "stop":
+    valg = str(input(f"Velg et av følgene valg:\n1.Addisjon\n2.Subtraksjon\n3.Multiplikasjon\n4.Divisjon\n5.Avslutt\nSkriv her: ")).lower()
+    if valg == "1" or valg == "addisjon":
+        tall1 = float(input("Skriv inn et tall: "))
+        print("Operator valgt: +")
+        tall2 = float(input("Skriv inn et tall til: "))
+        resultat = tall1 + tall2
+        print(f"{tall1} + {tall2} = {resultat}")
 
-if valg == 1:
-    tall1 = float(input("Skriv inn et tall: "))
-    print("+")
-    tall2 = float(input("Skriv inn et tall til: "))
-    resultat = tall1 + tall2
-    print(f"{tall1} + {tall2} = {resultat}")
-elif valg == 2:
-    tall1 = float(input("Skriv inn et tall: "))
-    print("-")
-    tall2 = float(input("Skriv inn et tall til: "))
-    resultat = tall1 - tall2
-    print(f"{tall1} - {tall2} = {resultat}")
-elif valg == 3:
-    tall1 = float(input("Skriv inn et tall: "))
-    print("*")
-    tall2 = float(input("Skriv inn et tall til: "))
-    resultat = tall1 * tall2
-    print(f"{tall1} * {tall2} = {resultat}")
-elif valg == 4:
-    tall1 = float(input("Skriv inn et tall: "))
-    print("/")
-    tall2 = float(input("Skriv inn et tall til: "))
-    resultat = tall1 / tall2
-    print(f"{tall1} / {tall2} = {resultat}")
-else:
-    pass
+    elif valg == "2" or valg == "subtraksjon":
+        tall1 = float(input("Skriv inn et tall: "))
+        print("Operator valgt: -")
+        tall2 = float(input("Skriv inn et tall til: "))
+        resultat = tall1 - tall2
+        print(f"{tall1} - {tall2} = {resultat}")
+
+    elif valg == "3" or valg == "multiplikasjon":
+        tall1 = float(input("Skriv inn et tall: "))
+        print("Operator valgt: *")
+        tall2 = float(input("Skriv inn et tall til: "))
+        resultat = tall1 * tall2
+        print(f"{tall1} * {tall2} = {resultat}")
+
+    elif valg == "4" or valg == "divisjon":
+        tall1 = float(input("Skriv inn et tall: "))
+        print("Operator valgt: /")
+        tall2 = float(input("Skriv inn et tall til: "))
+        resultat = tall1 / tall2
+        print(f"{tall1} / {tall2} = {resultat}")
+
+    elif valg == "avslutt":
+        print("Programmet avsluttes!")
+        break
+    
+    else:
+        print(f"Gjenskjenner ikke valg prøv på nytt!\n")
 
 
 input()
