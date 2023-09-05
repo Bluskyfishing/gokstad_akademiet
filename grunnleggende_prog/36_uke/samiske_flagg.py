7624fea45a9a4147161f24af5c7875790fb2b3ee
Bluskyfishing <blusky445@gmail.com>
2023-09-05 21:26:00 +0200

    Samisk flagg ferdig.

--- grunnleggende_prog/36_uke/samiske_flagg.py
+++ grunnleggende_prog/36_uke/samiske_flagg.py
@@ -6,7 +6,7 @@ t = turtle.Turtle()
 
 def rektangel():
     turtle.Screen().bgcolor("black")
-    t.speed(20)
+    t.speed(5)
     t.fillcolor("blue")
     t.begin_fill()
     t.up()
@@ -18,10 +18,79 @@ def rektangel():
         t.forward(300)
         t.left(90)
         t.forward(500)
-    
+    t.right(180)
     t.end_fill()
 
 rektangel()
 
-#Kryss ut applikasjon.
+t.forward(300)
+t.right(90)
+
+#Rød rektangel
+t.fillcolor("red")
+t.begin_fill()
+t.up()
+t.forward(300)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(300)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.end_fill()
+
+#Grønn stripe
+t.fillcolor("green")
+t.begin_fill()
+t.up()
+t.forward(300)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(120)
+t.right(180)
+t.forward(80)
+t.left(90)
+t.end_fill()
+
+#Gul stripe
+t.fillcolor("yellow")
+t.begin_fill()
+t.up()
+t.forward(300)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(300)
+t.right(90)
+t.forward(40)
+t.end_fill()
+
+#Sirkel blå halvdel
+t.width(15)
+t.left(180)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(25)
+
+t.pendown()
+t.pencolor("red")
+t.circle(100,180)
+t.pencolor("blue")
+t.circle(100,180)
+
+
+#Gjemme peker
+t.penup()
+t.fillcolor("black")
+t.pencolor("black")
+t.right(90)
+t.forward(100)
+
+#Kryss ut applikasjon
 turtle.exitonclick()
---
+++ grunnleggende_prog/36_uke/sirkel_test.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.width(10)
+
+
+
+
+
+t.pendown()
+t.pencolor("red")
+t.circle(100,180)
+t.pencolor("blue")
+t.circle(100,180)
+
+
+
+#exit window
+turtle.exitonclick()
