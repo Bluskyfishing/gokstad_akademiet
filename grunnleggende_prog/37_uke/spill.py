2dbd47b38bd185db4414887306c330027ce6439b
root <root@Fishas>
2023-09-11 11:30:17 +0200

    tuple, lists, while-for loops.

---
+++ grunnleggende_prog/37_uke/spill.py
@@ -0,0 +1,24 @@
+#import random
+#
+#tall = int(input("Skriv et tall: "))
+#forsok = 0 
+
+#while True:
+#    pc = random.randint(1,100)
+#    forsok += 1
+#    if tall == pc:
+#        print(f"Tallet er:{pc}\nAntall forsøk: {forsok}")
+#        break
+
+liste = [1,4,5,6,3,7]
+index = 0
+summering = 0 
+
+while True:
+    print(liste[index])
+    summering += liste[index]
+    index += 1
+
+    if index == len(liste):
+        print("Summen: ",summering)
+        break
