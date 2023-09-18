bb2eba9f244fb414168caca7465c821748a56434
Bluskyfishing <blusky445@gmail.com>
2023-09-18 20:58:45 +0200

    Quiz med while loop og funksjon.

--- grunnleggende_prog/38_uke/quiz.py
+++ grunnleggende_prog/38_uke/quiz.py
@@ -1,2 +1,21 @@
-right_answer = {}
+countries = {"Japan":"Tokyo","Norway":"Oslo","England":"London"}
 
+def quiz():
+    rounds = 0
+    points = 0
+
+    while True:
+        for i in countries.keys():
+            answer = input(f"Whats the capital in {i}?\n")
+            rounds += 1
+            if answer == countries[i]:
+                print("Correct! Next question!\n")   
+                points += 1    
+            else:
+                print("Wrong! Next question!\n")
+        if rounds == 3:
+            print(f"The quiz is over! You got {points} correct out of {rounds}.")
+            break
+
+
+quiz()
