e1f2512b85336f2a52f7e152dd38a2cf684f4fd9
root <root@Fishas>
2023-09-26 12:07:57 +0200

    Zip oppgave oppdatert.

--- grunnleggende_prog/39_uke/filter_zip_map.py
+++ grunnleggende_prog/39_uke/filter_zip_map.py
@@ -60,7 +60,7 @@ def negativeBeGone(x):
 #print(numgreaterThan10)
 
 
-#Bruk filter for å hente ut alle par-tall fra en dictionary med tall som nøkler. 
+#Bruk filter for å hente ut alle par-tall fra en dictionary med tall som nøkler. -----------
 
 def partall(x):
     if (x % 2) == 0:
@@ -68,6 +68,36 @@ def partall(x):
     else:
         return False
 
-partalldic = {"Isak":2,"Per":1,"Thorfinn":4}
-x = filter(partall,partalldic)
+#partalldic = {"Isak":2,"Per":1,"Thorfinn":4}
+#x = filter(partall,partalldic)
+#
+#for i in partalldic:
+#    print(i)    
 
+
+#Zip
+
+#Bruk zip for å kombinere listen ['a', 'b', 'c'] med listen [1, 2, 3] til en liste av tupler
+
+def ziplist():
+    mylist = ["a","b","c"]
+    mylist2 = [1,2,3,]
+    x = list(zip(mylist,mylist2))
+    print(x)
+
+
+#Kombiner to lister av tall der den ene er [1, 2, 3] og den andre er [4, 5, 6] 
+#til en ny liste med summer av parrede elementer ved hjelp av zip og map. 
+
+def zippedlist(list1):
+    return sum(list1)
+
+mylist = [1,2,3]
+mylist2 = [4,5,6]
+
+mycombinedlist = list(zip(mylist,mylist2))
+print(mycombinedlist)
+
+sum_list = map(zippedlist,mycombinedlist)
+for i in sum_list:
+    print(i)
