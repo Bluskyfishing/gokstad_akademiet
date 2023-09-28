a5d3db23093e34e868f0c41933d32c02b7352ea7
root <root@Fishas>
2023-09-28 10:28:46 +0200

    2 oppgaver gjenstår, en blir gjennomgått på tavla.

--- grunnleggende_prog/39_uke/sorted_reversed.py
+++ grunnleggende_prog/39_uke/sorted_reversed.py
@@ -6,7 +6,7 @@ def num_liste():
     num_list = [3, 1, 4, 1, 5, 9, 2, 6, 5]
     num_list.sort()
     print(num_list)
-   
+
 #Sorter listen ['apple', 'Banana', 'Cherry'] alfabetisk, uavhengig av store/små bokstaver. 
 
 def case_insensitive():
@@ -55,22 +55,17 @@ def reversed_tuple():
     mytuple = (1, 2, 3, 4, 5)
     print(tuple(reversed(mytuple)))
 
-#Hvordan ville du brukt reversed for å reversere nøklene i en dictionary? 
-
-def reversed_dict(): #lol?
-    mydict = {"Isak":"Kul","Brian":"Giga kul","Youko":"Japansk"}
-    mydict_reverse = list(reversed(mydict.keys()))
+#Hvordan ville du brukt reversed for å reversere nøklene i en dictionary? ----------
 
 #Reverser rekkefølgen av ord i strengen 'Python er gøy' ved hjelp av reversed.
- 
 def reverseKeys(dictionary):
     for key in list(dictionary.keys()):
         reverseKey = key[::-1]
         value = dictionary.pop(key)
         dictionary[reverseKey] = value
-dic = {"isak": 1, "arnas": 2}
-reverseKeys(dic)
-print(dic)
+#dic = {"isak": 1, "arnas": 2}
+#reverseKeys(dic)
+#print(dic)
 
 
 def reversed_string():
