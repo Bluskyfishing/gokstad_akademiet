4ebd0590819c90e8fd11cd48704c79be9e678a81
Bluskyfishing <blusky445@gmail.com>
2023-10-13 14:14:56 +0200

    Done with weather station task.

--- grunnleggende_prog/40_uke/weather_station.py
+++ grunnleggende_prog/40_uke/weather_station.py
@@ -1,9 +1,8 @@
-def addData(date,temprature,stateOfWeather):
-    newdata = {"Date": date,"Temperature": temprature, "State:": stateOfWeather}
-    listOfDict.append(newdata)
-    return listOfDict
+def addData(list,date,temp,stateOfWeather):
+    newdata = {"Date":date,"Temp":temp, "State":stateOfWeather}
+    list.append(newdata)
 
-def devideList(list,n=6):
+def devideList(list,n=3):
     for i in range(0, len(list), n):
         yield list[i:i + n]
     
@@ -11,19 +10,17 @@ def prettyPrintOfDict(dict):
     for index in range(0,3):
         keypairs = []
         for i in dict:
-            Key, Value = list(i.items())[index]
-            keypairs.append(Key)
-            keypairs.append(Value)
+            key, value = list(i.items())[index]
+            keypairs.append(key)
+            keypairs.append(value)
         for i in range(0, len(keypairs), 2):
-                print(str(keypairs[i]).center(10), ":" ,str(keypairs[i+1]).center(10), "|" ,end="")
+                key = str(keypairs[i]).center(10)
+                value = str(keypairs[i+1]).center(10)
+                print(key, ":", value, "|",end="")
         print()
 
 def mainMenu():
-    global listOfDict 
-    listOfDict = [{"Date": "12/12/2012","Temprature": 12, "State:": "sunny"},
-                  {"Date": "13/12/2013","Temprature": 13, "State:": "rainy"},
-                  {"Date": "14/12/2022","Temprature": 14, "State:": "cloudy"},
-                  {"Date": "15/12/2023","Temprature": 15, "State:": "sunny"}]
+    listOfDict = []
 
     while True:
         inp = input("\na. Add weather data.\nb. Show saved information.\nc. Analyze data.\nd. Quit.\nInput: ").lower()
@@ -37,37 +34,37 @@ def mainMenu():
                 print("Invalid type! Temperature must be a number. Please try again!")
                 continue
 
-            addData(date,temp,state)
+            addData(listOfDict,date,temp,state)
+
         if inp == "b":
-            if len(listOfDict) > 6:
-                for part in devideList(listOfDict, 6):
+            if len(listOfDict) > 3:
+                for part in devideList(listOfDict, 3):
                     print()
                     prettyPrintOfDict(part)
             else:
                 prettyPrintOfDict(listOfDict)
-            print(listOfDict)
+
         if inp == "c":
             averageTempList = []
             total = 0
+            try: 
+                for i in range(len(listOfDict)):
+                    elementInList = listOfDict[i]
+                    valuesFromList = list(elementInList.values())
+                    averageTempList.append(valuesFromList[1])
+                for i in averageTempList:
+                    total += i
 
-            for i in range(len(listOfDict)):
-                elementInList = listOfDict[i]
-                valuesFromList = list(elementInList.values())
-                averageTempList.append(valuesFromList[1])
-            for i in averageTempList:
-                total += i
+                averageTemp = total/len(averageTempList)
+                highestTemp = max(averageTempList)
+                lowestTemp = min(averageTempList)
+                print(f"\n---Statistics from data collected---\nAverage Temperature:{averageTemp}")
+                print(f"Highest Temperature:{highestTemp}\nLowest Temperature:{lowestTemp}")
+            except ZeroDivisionError:
+                print("No data is stored! Add some data to be analysed!")
 
-            averageTemp = total/len(averageTempList)
-            highestTemp = max(averageTempList)
-            lowestTemp = min(averageTempList)
-            print(f"\n---Statistics from data collected---\nAverage Temperature:{averageTemp}")
-            print(f"Highest Temperature:{highestTemp}\nLowest Temperature:{lowestTemp}")
         if inp == "d":
             print("\nQuitting program!")
             break
 
 mainMenu()
-
-
-{'Date': '12/12/2001', 'Temperature': 12, 'State:': 'sunny'}
-{'Date': '15/12/2023', 'Temprature': 15, 'State:': 'sunny'}
