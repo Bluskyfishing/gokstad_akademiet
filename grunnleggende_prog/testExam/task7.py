07fa6f804f7bedc4edbef9c4ee3cb690d74adbe6
Bluskyfishing <blusky445@gmail.com>
2023-11-25 22:15:32 +0100

    final tweaks

--- grunnleggende_prog/testExam/task7.py
+++ grunnleggende_prog/testExam/task7.py
@@ -4,29 +4,32 @@ def questionMaker(fileName, questionAmount):
     with open(fileName, "w") as writeFile:
         for i in range(questionAmount):
                 mathLength = rnd.randint(2,7)
-                for itere in range(mathLength):
+                for number in range(mathLength):
                     num = rnd.randint(1,101)
                     operatorInt = rnd.randint(0,1)
                     operatorList = ["-","+"]
                     writeFile.write(f"{num} ")
-                    if itere == mathLength -1:
+                    if number == mathLength -1:
                         continue
                     writeFile.write(f"{operatorList[operatorInt]} ")
                 writeFile.write("=")
-                writeFile.write("\n") #Vil remove siste newLine
+                if i == questionAmount -1:
+                    continue
+                writeFile.write("\n")
 
-def copyFile(inputFile, outputFile):
-    with open(inputFile, "r") as readFile:
+def copyFile(file, outputFile):
+    with open(file, "r") as readFile:
         data = readFile.read()
 
     with open(outputFile, "w") as writeFile:
         writeFile.write(data)
         writeFile.close()
 
-def mathSolver(fileName):
-    with open(fileName, "r") as readFile:
+def mathSolver(file):
+    with open(file, "r") as readFile:
         expressions = []
         results = []
+        
         for line in readFile.readlines():
             lineList = line.split()
             expressions.append(lineList)
@@ -50,30 +53,30 @@ def mathSolver(fileName):
 
     return expressions, results
 
+def solutionPrint(file):
+    solverExpressions, solverResults = mathSolver(file)
+
+    for i in range(len(solverExpressions)):
+        solverExpressions[i].append(solverResults[i])
+
+    with open(file, "w") as writeFile:
+        for itemList in solverExpressions:
+            for item in itemList:
+                writeFile.writelines(str(item) )
+                writeFile.writelines(" ")
+            if itemList == solverExpressions[-1]:
+                continue
+            writeFile.writelines("\n")
+        writeFile.close() 
+
+def mathQuestionBuilder(file, questionAmount, outputFile, ):
+    questionMaker(file, questionAmount)
+    copyFile(file, outputFile)
+    mathSolver(file)
+    solutionPrint(outputFile)
 
 FILENAME = "regnestykker.txt"
-questionAmount = rnd.randint(1,10) # 10 HUSK! endre tilbake 
+questionAmount = rnd.randint(1,10) 
 SOLVEDFILE = "regnestykker_løsning.txt"
 
-questionMaker(FILENAME, questionAmount)
-copyFile(FILENAME, SOLVEDFILE)
-mathSolver(SOLVEDFILE)
-
-solverExpressions, solverResults = mathSolver(SOLVEDFILE) #Få det siste i en func? Lage en stor func?
-for i in range(len(solverExpressions)):
-    solverExpressions[i].append(solverResults[i])
-
-with open(SOLVEDFILE, "w") as writeFile:
-    for itemList in solverExpressions:
-        for item in itemList:
-            writeFile.writelines(str(item))
-            writeFile.writelines(" ")
-        writeFile.writelines(" \n")
-    writeFile.close() 
-
-
-
-#def mathQuestionBuilder(inputFile, outputFile, questionAmount):
-#    questionMaker(inputFile, questionAmount)
-#    copyFile(inputFile, outputFile)
-#    mathSolver(outputFile)
+mathQuestionBuilder(FILENAME, questionAmount, SOLVEDFILE)
