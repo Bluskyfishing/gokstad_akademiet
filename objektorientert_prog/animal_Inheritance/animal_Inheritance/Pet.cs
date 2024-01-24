5d673f31a075550c5bc30616e942510079ff5384
Bluskyfishing <blusky445@gmail.com>
2024-01-24 13:17:54 +0100

    Assignment 3: Pets. Inheritence

---
+++ objektorientert_prog/animal_Inheritance/animal_Inheritance/Pet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace animal_Inheritance {
+
+    public class Pet {
+        public string name;
+        public string noise;
+        public string trick;
+        public Pet(string name, string noise, string trick) {
+            this.name = name;
+            this.noise = noise;
+            this.trick = trick;
+        }
+
+            public virtual void GetName() {
+                Console.WriteLine("Pet Name: " + name);
+            }
+            public virtual void GetNoise() {
+                Console.WriteLine("The animal makes a noise: " + noise);
+            }
+            public virtual void GetTrick() {
+                Console.WriteLine("The animal does a trick: " + trick);
+            }
+    }
+    public class Dog(string name) : Pet(name, "Woof", "*Fetches ball*") {
+    
+    }
+    public class Cat(string name) : Pet(name, "Meow", "*sleeps*") {
+
+    }
+    public class Fish(string name) : Pet(name, "Blub", "*Jumps out of water*") {
+
+    }
+}
---
+++ objektorientert_prog/animal_Inheritance/animal_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using animal_Inheritance;
+using System;
+using System.Collections.Generic;
+
+namespace animal_inheritance{
+    internal class Program {
+        static void Main(string[] args) {
+            List<Pet> pets = new List<Pet>();
+
+            pets.Add(new Dog("Bjarne"));
+            pets.Add(new Dog("Bjarte"));
+            pets.Add(new Cat("Kalle"));
+            pets.Add(new Cat("Kristian"));
+            pets.Add(new Fish("Oscar"));
+            pets.Add(new Fish("Ole"));
+            
+
+            foreach (Pet pet in pets) {
+                pet.GetName();
+                pet.GetNoise();
+                pet.GetTrick();
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
