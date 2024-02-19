e287ab56063b0648c4a42fde015f31302f4da850
Bluskyfishing <blusky445@gmail.com>
2024-02-19 23:38:19 +0100

    Error handeling implementet.

--- objektorientert_prog/TextDecorator/TextDecorator/Program.cs
+++ objektorientert_prog/TextDecorator/TextDecorator/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics.Metrics;
 using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters;
 
 namespace TextDecorator
@@ -26,7 +27,18 @@ namespace TextDecorator
             string[] arguments = Environment.GetCommandLineArgs();
             if (arguments.Length == 1)
             {
-                Console.WriteLine("No arguments given!");
+                Console.WriteLine("No arguments given! Try the help command!");
+            }
+            else if (arguments.Length == 2)
+            {
+                if (arguments[1].ToLower() == "help")
+                {
+                    Console.WriteLine($"Command syntax: <file> <Decorator> <Text>");
+                }
+                else
+                {
+                    Console.WriteLine("Too few arguments or unknown argument! Try the help command!");
+                }
             }
             else if (arguments.Length == 3) 
             {
@@ -46,7 +58,7 @@ namespace TextDecorator
                     }
                 }
 
-                if (arguments[1].ToLower() == "alt") //alternative caps
+                else if (arguments[1].ToLower() == "alt") //alternative caps
                 {
                     int index = 1;
                     foreach (char letter in arguments[2]) 
@@ -73,11 +85,11 @@ namespace TextDecorator
                 char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
                 char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-                if (arguments[1].ToLower() == "pig") //Pig latin
+                else if (arguments[1].ToLower() == "pig") //Pig latin
                 {
                     string[] words = arguments[2].Split(' ');
                     foreach (string word in words)
-                    {  
+                    {
                         if (vowels.Contains(Char.ToLower(word[0])))
                         {
                             Console.Write(word + "yay" + " ");
@@ -93,35 +105,34 @@ namespace TextDecorator
                                     break;
                                 }
 
-                                else if (consonants.Contains(Char.ToLower(letter)))  
+                                else if (consonants.Contains(Char.ToLower(letter)))
                                 {
                                     startingLetters.Add(letter);
                                 }
                             }
 
                             string strStartLetters = string.Join("", startingLetters);
-                            
+
                             if (Char.IsUpper(strStartLetters[0]))
                             {
-                                string capitalizeWord = Char.ToUpper(word.Substring(startingLetters.Count)[0]) + word.Substring(startingLetters.Count +1);
+                                string capitalizeWord = Char.ToUpper(word.Substring(startingLetters.Count)[0]) + word.Substring(startingLetters.Count + 1);
                                 string uncapitalizeWord = Char.ToLower(strStartLetters[0]) + strStartLetters.Substring(1);
-                                Console.WriteLine(capitalizeWord);
                                 string completeWord = capitalizeWord + uncapitalizeWord;
                                 char symbol = SymbolFinder(completeWord, vowels, consonants);
 
-                                if (symbol == 'x') 
+                                if (symbol == 'x')
                                 {
                                     Console.Write(completeWord + "ay" + " ");
                                 }
                                 else
                                 {
                                     string removedSymbol = completeWord.Remove(completeWord.IndexOf(symbol), 1);
-                                    Console.Write(removedSymbol + "ay"+ symbol + " ");
+                                    Console.Write(removedSymbol + "ay" + symbol + " ");
                                 }
                             }
                             else
                             {
-                                string firstHalf =  word.Substring(startingLetters.Count);
+                                string firstHalf = word.Substring(startingLetters.Count);
                                 string secondHalf = strStartLetters;
                                 string completeWord = firstHalf + secondHalf;
                                 char symbol = SymbolFinder(completeWord, vowels, consonants);
@@ -138,6 +149,10 @@ namespace TextDecorator
                             }
                         }
                     }
+                }
+                else if (arguments.Length == 3)
+                {
+                    Console.WriteLine("Unknown decorator used! Try the help command!");
                 }
             }
         }
