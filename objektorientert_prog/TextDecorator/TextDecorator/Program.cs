7635379145b7b5e426c6b9abbb6f4f47c7e1e3bd
Bluskyfishing <blusky445@gmail.com>
2024-02-21 16:17:01 +0100

    y can now be a vowel. Multiple comments added, and readabilty improved.

--- objektorientert_prog/TextDecorator/TextDecorator/Program.cs
+++ objektorientert_prog/TextDecorator/TextDecorator/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics.Tracing;
 using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TextDecorator
 {
@@ -26,16 +27,18 @@ namespace TextDecorator
         static void Main(string[] args)
         {
             string[] arguments = Environment.GetCommandLineArgs(); //Array with filepath and arguments if any.
-
+            
             if (arguments.Length == 1)
             {
                 Console.WriteLine("No arguments given! Try the help command!");
             }
             else if (arguments.Length == 2)
             {
-                if (arguments[1].ToLower() == "help")
+                string decorator = arguments[1].ToLower();
+
+                if (decorator.ToLower() == "help")
                 {
-                    Console.WriteLine($"Command syntax: <file> <Decorator> <Text>");
+                    Console.WriteLine($"Command syntax: <file> <Decorator> <Text>\nAvailable decorators: '#', 'alt', 'pig'");
                 }
                 else
                 {
@@ -47,7 +50,7 @@ namespace TextDecorator
                 string decorator = arguments[1].ToLower();
                 string text = arguments[2];
 
-                if (decorator == "#") //block mode
+                if (decorator == "#") //Block mode
                 {
                     int counter = 0;
 
@@ -57,19 +60,19 @@ namespace TextDecorator
                         if (i == text.Length +1 && counter == 0) 
                         {
                             Console.WriteLine($"\n#{text}#");
-                            counter++; //continue the loop for the floor of the box.
+                            counter++; //Continue the loop for the floor of the box.
                             i = -1; //Sets to -1 to finish box
                         }
                     }
                 }
 
-                else if (decorator == "alt") //alternating caps
+                else if (decorator == "alt") //Alternating caps
                 {
                     int index = 1;
 
                     foreach (char letter in text) 
                     {
-                        if (letter == ' ') //blankspace doesnt affect next upper/lower case. 
+                        if (letter == ' ') //Blankspace doesnt affect next upper/lower case. 
                             Console.Write(letter);
                         else if (index % 2 == 0)
                         {
@@ -81,7 +84,7 @@ namespace TextDecorator
                             Console.Write(char.ToUpper(letter));
                             index++;
                         }
-                        else //prints if text is not a letter. 
+                        else //Prints if text is not a letter. 
                         { 
                             Console.Write(letter); 
                         }
@@ -93,7 +96,7 @@ namespace TextDecorator
                 {
                     char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
                     char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-                    string[] wordArray = text.Split(' '); //splits texts string by blankspaces
+                    string[] wordArray = text.Split(' '); //Splits text string by blankspaces
 
                     foreach (string word in wordArray)
                     {
@@ -101,16 +104,22 @@ namespace TextDecorator
 
                         if (vowels.Contains(firstLetter)) //Checks if firstletter is a vowel.
                         {
-                            Console.Write(word + "yay" + " ");
+                            Console.Write(word + "yay" + " "); 
                         }
                         else if (consonants.Contains(firstLetter)) //Check if firstletter is a consonant.
                         {
                             List<char> startingLetters = new List<char>();
+
                             bool upperCase = false;
 
                             foreach (char letter in word) //Checks for consonant clusters.
                             {
-                                if (vowels.Contains(char.ToLower(letter)))
+                                if (letter == 'y' && word.IndexOf(letter) == 1) //If y is in the second position it is counted as a vowel.
+                                {
+                                    break;
+                                }
+
+                                if (vowels.Contains(char.ToLower(letter))) //Go
                                 {
                                     break;
                                 }
@@ -119,40 +128,40 @@ namespace TextDecorator
                                 {
                                     if (char.IsUpper(letter))
                                     {
-                                        upperCase = true;
+                                        upperCase = true; //Flags word, if it contains a capital letter.
                                     }
                                     startingLetters.Add(letter);
                                 }
+
                             }
 
-                            string strStartLetters = string.Join("", startingLetters);
-                            string pigLatinWord = word.Substring(strStartLetters.Length) + strStartLetters.ToLower() + "ay";
+                            string strStartLetters = string.Join("", startingLetters); //Creates a string of the consonant cluster.
+                            string pigLatinWord = word.Substring(strStartLetters.Length) + strStartLetters.ToLower() + "ay"; //Creates a string out of the cluster and rest of the word + ay
+                            char symbol = SymbolFinder(word, vowels, consonants); //Returns symbol if exists.
 
-                            char symbol = SymbolFinder(word, vowels, consonants);
-
-                            if (upperCase)
+                            if (upperCase) //If capitalized word
                             {
                                 if (symbol != 'x')
                                 {
-                                    string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1);
-                                    Console.Write(char.ToUpper(pigLatinNoSymbol[0]) + pigLatinNoSymbol.Substring(1) + symbol + " ");
+                                    string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1); //Finds the symbol and removes it from the string. 
+                                    Console.Write(char.ToUpper(pigLatinNoSymbol[0]) + pigLatinNoSymbol.Substring(1) + symbol + " "); //Capitalizes first letter and puts rest of word together with the symbol.
                                 }
                                 else
                                 {
-                                    Console.Write(char.ToUpper(pigLatinWord[0]) + pigLatinWord.Substring(1) + " ");
+                                    Console.Write(char.ToUpper(pigLatinWord[0]) + pigLatinWord.Substring(1) + " "); //Does the same without symbol.
 
                                 }
                             }
-                            else
+                            else //If uncapitalized word
                             {
                                 if (symbol != 'x')
                                 {
-                                    string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1);
-                                    Console.Write(pigLatinNoSymbol + symbol + " ");
+                                    string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1); //Finds the symbol and removes it from the string. 
+                                    Console.Write(pigLatinNoSymbol + symbol + " "); //Writes the word with the symbol.
                                 }
                                 else
                                 {
-                                    Console.Write(pigLatinWord + " ");
+                                    Console.Write(pigLatinWord + " "); //If word is not capitalized or has symbol, prints word.
                                 }
                             } 
                         }
