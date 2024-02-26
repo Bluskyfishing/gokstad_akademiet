7df7da1375885d57931020b9f5cd06cc333d7439
Bluskyfishing <blusky445@gmail.com>
2024-02-26 12:49:42 +0100

    palindrome task, not finished

--- objektorientert_prog/typeTest/typeTest/Program.cs
+++ objektorientert_prog/typeTest/typeTest/Program.cs
@@ -21,6 +21,43 @@ namespace typTest
             string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
             string castedCars= string.Join(" ", cars.Skip(2));
             Console.WriteLine(castedCars);
+
+            //Palindrome
+
+            List<string> palindromes = new List<string>()
+            {
+                "lol",
+                "Racecar",
+                "Never odd or even"
+            };
+
+
+            foreach (string palindrome in palindromes)
+            {
+                Console.WriteLine($"{palindrome}");
+                Console.WriteLine(ChecksIfPalindrome(palindrome));
+            }
+
+        }
+        public static bool ChecksIfPalindrome(string input)
+        {
+            //lol
+            string lowerInput = input.ToLower();
+            Array arrayReverse = lowerInput.ToCharArray();
+            Array.Reverse(arrayReverse);
+            string reverseWord = arrayReverse.ToString();
+            Console.WriteLine(reverseWord);
+
+            if (lowerInput == reverseWord)
+            {
+                Console.WriteLine("It is a palindrome!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("It is NOT a Palindrome!");
+                return false;
+            }
         }
     }
 }
