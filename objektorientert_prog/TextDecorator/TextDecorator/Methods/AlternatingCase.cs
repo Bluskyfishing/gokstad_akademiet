839551b03c5152b0b76077e831e9d5a335ae46b1
Bluskyfishing <blusky445@gmail.com>
2024-02-26 20:37:29 +0100

    Separated each option to different methods and files.

---
+++ objektorientert_prog/TextDecorator/TextDecorator/Methods/AlternatingCase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TextDecorator.MethodAlt
+{
+    internal class AlternatingCaseClass
+    {
+        public static void AlternateCaseMode(string text)
+        {
+            int index = 1;
+
+            foreach (char letter in text)
+            {
+                if (letter == ' ') //Blankspace doesnt affect next upper/lower case. 
+                    Console.Write(letter);
+                else if (index % 2 == 0)
+                {
+                    Console.Write(char.ToLower(letter));
+                    index++;
+                }
+                else if (index % 2 != 0)
+                {
+                    Console.Write(char.ToUpper(letter));
+                    index++;
+                }
+                else //Prints if text is not a letter. 
+                {
+                    Console.Write(letter);
+                }
+            }
+        }
+    }
+}
---
+++ objektorientert_prog/TextDecorator/TextDecorator/Methods/PigLatin.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextDecorator.Symbolfinder;
+
+namespace TextDecorator.MethodPig
+{
+    public static class PigLatinClass
+    {
+        public static void PigLatinMode(string[] text)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach (string word in text)
+            {
+                char firstLetter = char.ToLower(word[0]);
+
+                if (vowels.Contains(firstLetter)) //Checks if firstletter is a vowel.
+                {
+                    Console.Write(word + "yay" + " ");
+                }
+                else if (consonants.Contains(firstLetter)) //Check if firstletter is a consonant.
+                {
+                    List<char> startingLetters = new List<char>();
+
+                    bool upperCase = false;
+
+                    foreach (char letter in word) //Checks for consonant clusters.
+                    {
+                        if (letter == 'y' && word.IndexOf(letter) == 1) //If y is in the second position it is counted as a vowel.
+                        {
+                            break;
+                        }
+
+                        if (vowels.Contains(char.ToLower(letter))) //Go
+                        {
+                            break;
+                        }
+
+                        else if (consonants.Contains(char.ToLower(letter)))
+                        {
+                            if (char.IsUpper(letter))
+                            {
+                                upperCase = true; //Flags word, if it contains a capital letter.
+                            }
+                            startingLetters.Add(letter);
+                        }
+
+                    }
+
+                    string strStartLetters = string.Join("", startingLetters); //Creates a string of the consonant cluster.
+                    string pigLatinWord = word.Substring(strStartLetters.Length) + strStartLetters.ToLower() + "ay"; //Creates a string out of the cluster and rest of the word + ay
+                    char symbol = ListCompareClass.ListsComparison(word, vowels, consonants); //Returns symbol if exists.
+
+                    if (upperCase) //If capitalized word
+                    {
+                        if (symbol != 'x')
+                        {
+                            string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1); //Finds the symbol and removes it from the string. 
+                            Console.Write(char.ToUpper(pigLatinNoSymbol[0]) + pigLatinNoSymbol.Substring(1) + symbol + " "); //Capitalizes first letter and puts rest of word together with the symbol.
+                        }
+                        else
+                        {
+                            Console.Write(char.ToUpper(pigLatinWord[0]) + pigLatinWord.Substring(1) + " "); //Does the same without symbol.
+
+                        }
+                    }
+                    else //If uncapitalized word
+                    {
+                        if (symbol != 'x')
+                        {
+                            string pigLatinNoSymbol = pigLatinWord.Remove(pigLatinWord.IndexOf(symbol), 1); //Finds the symbol and removes it from the string. 
+                            Console.Write(pigLatinNoSymbol + symbol + " "); //Writes the word with the symbol.
+                        }
+                        else
+                        {
+                            Console.Write(pigLatinWord + " "); //If word is not capitalized or has symbol, prints word.
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
