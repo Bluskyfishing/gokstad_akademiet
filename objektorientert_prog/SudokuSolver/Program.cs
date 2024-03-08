9fafc343f7aed680f69cc5ec93eb7d06d1699c74
Bluskyfishing <blusky445@gmail.com>
2024-03-08 14:32:51 +0100

    remaking code, should only test if valid, not solve lol

--- objektorientert_prog/SudokuSolver/Program.cs
+++ objektorientert_prog/SudokuSolver/Program.cs
@@ -8,42 +8,36 @@ namespace SudokuSolver
         {
             public bool IsValidSudoku(char[][] board)
             {
-                bool result = false;
-                bool charCheck = false;
-                char[][] permutation;
-                int counter = 1; 
-
-                if (board.Length == 9)
+                if (board.Length == 9 )
                 {
-                    while (true)
+                    for ( int i = 0; i < board.Length; i++ )
                     {
-                        counter++;
-                        if (counter == 3)
+                        if (board[i].Length == 9 )
                         {
-                            break;
-                        }
-                        for (int i = 0; i < board.Length; i++)
-                        {
-                            Console.WriteLine(board[i]);
-                            if (board[i].Length == 9) //first check list if so it doesnt have any repeating numbers.
-                            {
-                                for (int j = 0; j < board[i].Length; j++) //1-9 generator, checks if in array, and if number is in same index.
+                            for (int j = 0; j < board[i].Length; j++) 
+                            {                                         
+                                if (Char.IsDigit(board[i][j]) || board[i][j] == '.')
                                 {
-                                    Console.WriteLine(board[i][j]);
+                                    if (Array.IndexOf(board[i], board[i][j]) >= 0) //check if number is in row.(vertical)
+                                    {
+                                        return false;
+                                    }
+                                    if (Array.IndexOf(board[j], board[j][i]) >= 0) //check if number is in column. (horizontal)
+                                    {
+                                        return false;
+                                    } 
                                 }
 
                             }
                         }
                     }
-
                 }
-                return result;
             }
         }
         static void Main(string[] args)
         {
             char[][] board = 
-                [['5', '3', '.', '.', '7', '.', '.', '.', '7'], 
+                [['5', '3', '.', '.', '7', '.', '.', '.', '.'], 
                 ['6', '.', '.', '1', '9', '5', '.', '.', '.'], 
                 ['.', '9', '8', '.', '.', '.', '.', '6', '.'], 
                 ['8', '.', '.', '.', '6', '.', '.', '.', '3'], 
@@ -53,9 +47,21 @@ namespace SudokuSolver
                 ['.', '.', '.', '4', '1', '9', '.', '.', '5'], 
                 ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
 
+            char[][] board2 =
+                [['8', '3', '.', '.', '7', '.', '.', '.', '.'],
+                ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+                ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+                ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+                ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+                ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+                ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+                ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+                ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
+
             Solution  solution = new Solution();
 
             Console.WriteLine(solution.IsValidSudoku(board));
+            Console.WriteLine(solution.IsValidSudoku(board2));
         }
     }
 }
