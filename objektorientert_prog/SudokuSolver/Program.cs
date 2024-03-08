6f5f89621dd481a0e7f529e64afe70cf2abeab24
Bluskyfishing <blusky445@gmail.com>
2024-03-08 23:48:27 +0100

    Not fully implemented 3x3 validation.

--- objektorientert_prog/SudokuSolver/Program.cs
+++ objektorientert_prog/SudokuSolver/Program.cs
@@ -1,13 +1,49 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-namespace SudokuSolver
+namespace SudokuValidator
 {
     internal class Program
     {
         public class Solution
         {
+            public bool SubBoxChecker(char[][] board, int start)
+            {
+                List<char[]> tempList = new List<char[]>();
+                List<char[]> listbox = new List<char[]>();
+                List<char> resultList = new List<char>();
+
+
+                foreach (char[] row in board)
+                {
+                    tempList.Add(row.Take(start).ToArray());
+                }
+
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    listbox.Add(tempList[i]);
+                    if (listbox.Count() == 3)
+                    {
+                        for (int j = 0; j < listbox.Count(); j++)
+                        {
+                            for (int k = 0; k < listbox[j].Count(); k++)
+                            {
+                                if (listbox[j][k] == '.') continue;
+                                resultList.Add(listbox[j][k]);
+                            } 
+                        }
+                        foreach (char row in resultList)
+                        {
+                            Console.WriteLine(row);
+                        }
+                        resultList.Clear();
+                    } 
+                }
+
+                return true;
+            }
             public bool IsValidSudoku(char[][] board)
             {
                 for (int i = 0; i < board.Length; i++)
@@ -31,7 +67,7 @@ namespace SudokuSolver
                             return false;
                         }
 
-                        if (!listWithoutDuplicates2.Contains(board[j][i])) //Checks row if duplicate.
+                        if (!listWithoutDuplicates2.Contains(board[j][i])) //Checks column if duplicate.
                         {
                             if (board[j][i] != '.')
                             {
@@ -43,6 +79,8 @@ namespace SudokuSolver
                         {
                             return false;
                         }
+
+
                     }
                 }
                 return true;
@@ -72,22 +110,12 @@ namespace SudokuSolver
                 ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
                 ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
 
-            char[][] board3 =
-                [['8', '3', '.', '.', '7', '.', '.', '.', '.'], //FALSE
-                ['6', '8', '.', '1', '9', '5', '.', '.', '.'],
-                ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-                ['.', '.', '.', '.', '6', '.', '.', '.', '3'],
-                ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-                ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-                ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-                ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-                ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
-
             Solution  solution = new Solution();
 
             Console.WriteLine(solution.IsValidSudoku(board));
             Console.WriteLine(solution.IsValidSudoku(board2));
-            Console.WriteLine(solution.IsValidSudoku(board3));
+
+            solution.SubBoxChecker(board, 3);
         }
     }
 }
