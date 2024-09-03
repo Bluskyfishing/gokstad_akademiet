ca966d27cd3d51ea740cdf3811a1d947e784bce6
Bluskyfishing <blusky445@gmail.com>
2024-09-03 11:56:32 +0200

    Added bubblesort and selectionsort

---
+++ algoritmiske_metoder/DifferentSorts/bubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using bubbleSort;
+
+int[] sort(int[] input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        for (int j = i; j < input.Length; j++)
+        {
+            if (input[i] > input[j])
+            {
+                int temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+    }
+    return input;
+}
+
+int[] theNumbers = { 15, 123, 157, 56, 4, 171, 188, 42, 219, 107, 139, 253, 91, 23, 8, 204, 72, 148, 234, 16 };
+
+Console.WriteLine("Unsorted: " + sortUtils.isSorted(theNumbers));
+foreach (int i in theNumbers)
+{
+    Console.Write(i + " ");
+}
+
+int[] sortedNumbers = sort(theNumbers);
+Console.WriteLine("\n\nSorted: " + sortUtils.isSorted(sortedNumbers));
+foreach (int i in sortedNumbers)
+{
+    Console.Write(i + " ");
+}
---
+++ algoritmiske_metoder/DifferentSorts/selectionSort/selectionSort/sortUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selectionSort
+{
+    internal class sortUtils
+    {
+        public static bool isSorted(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length  - 1; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+}
